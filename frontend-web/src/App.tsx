cb80e1d0162c374a2cd52e993b519bf926e231e6
pjm6857 <pjm6857@gmail.com>
2026-01-30 17:46:49 +0900

    feat: 시세정보 css

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 
-// [변경] 시세 관련 페이지들을 'market' 폴더에서 불러옵니다.
+// 시세 관련 페이지
 import MarketPage from './pages/markets/MarketPage';
 import ItemDetailPage from './pages/markets/ItemDetailPage';
 
+// 도구 관련 페이지
 import GeneralReforgePage from './pages/tools/GeneralReforgePage';
 import AdvancedReforgePage from './pages/tools/AdvancedReforgePage';
 import AbydosPage from './pages/tools/AbydosPage';
@@ -17,13 +18,14 @@ function App() {
                 <Route element={<Layout />}>
                     <Route path="/" element={<Home />} />
 
-                    {/* 시세 정보: 리스트 */}
+                    {/* [수정됨] 시세 정보: 리스트 */}
                     <Route path="/market/:categoryId" element={<MarketPage />} />
 
-                    {/* 시세 정보: 상세 (그래프/예측) */}
+                    {/* [수정됨] 시세 정보: 상세 (그래프/예측) */}
                     <Route path="/market/detail/:itemName" element={<ItemDetailPage />} />
 
-                    <Route path="/market" element={<Navigate to="/" replace />} />
+                    {/* [핵심 수정] /market 접속 시 홈(/)이 아니라 '재련 재료'(/market/reforge) 탭으로 이동 */}
+                    <Route path="/market" element={<Navigate to="/market/reforge" replace />} />
 
                     {/* 도구 라우팅 */}
                     <Route path="/tools" element={<Navigate to="/tools/general" replace />} />
